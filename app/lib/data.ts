32ec68cd2cb1cd85b7cdcc65a696328974115fbd
theGUI001 <lghmelo@gmail.com>
2023-11-07 17:26:45 -0300

    feat(customers): added customers page

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -236,6 +236,69 @@ export async function fetchFilteredCustomers(query: string) {
 		throw new Error("Failed to fetch customer table.");
 	}
 }
+export async function fetchFilteredCustomersWithPagination(
+	query: string,
+	currentPage: number,
+) {
+	noStore();
+
+	const offset = (currentPage - 1) * ITEMS_PER_PAGE;
+
+	try {
+		const data = await sql<CustomersTable>`
+		SELECT
+		  customers.id,
+		  customers.name,
+		  customers.email,
+		  customers.image_url,
+		  COUNT(invoices.id) AS total_invoices,
+		  SUM(CASE WHEN invoices.status = 'pending' THEN invoices.amount ELSE 0 END) AS total_pending,
+		  SUM(CASE WHEN invoices.status = 'paid' THEN invoices.amount ELSE 0 END) AS total_paid
+		FROM customers
+		LEFT JOIN invoices ON customers.id = invoices.customer_id
+		WHERE
+		  customers.name ILIKE ${`%${query}%`} OR
+        customers.email ILIKE ${`%${query}%`}
+		GROUP BY customers.id, customers.name, customers.email, customers.image_url
+		ORDER BY customers.name ASC
+		LIMIT ${ITEMS_PER_PAGE} OFFSET ${offset}
+	  `;
+
+		const customers = data.rows.map((customer) => ({
+			...customer,
+			total_pending: formatCurrency(customer.total_pending),
+			total_paid: formatCurrency(customer.total_paid),
+		}));
+
+		return customers;
+	} catch (err) {
+		console.error("Database Error:", err);
+		throw new Error("Failed to fetch customer table.");
+	}
+}
+
+export default async function fetchCustomersPage(query: string) {
+	noStore();
+
+	try {
+		const count = await sql`
+		SELECT
+		  COUNT(*)
+		FROM customers
+		WHERE
+		  customers.name ILIKE ${`%${query}%`} OR
+        customers.email ILIKE ${`%${query}%`}
+	  `;
+
+		const totalPages = Math.ceil(
+			Number(count.rows[0].count) / ITEMS_PER_PAGE,
+		);
+		return totalPages;
+	} catch (error) {
+		console.error("Database Error:", error);
+		throw new Error("Failed to fetch total number of customers.");
+	}
+}
 
 export async function getUser(email: string) {
 	try {
