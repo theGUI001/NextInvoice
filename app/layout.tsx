06e72dac08b0da39d4716d8cbb31d26922eb5a56
theGUI001 <lghmelo@gmail.com>
2023-11-08 16:29:45 -0300

    feat: added metadata in pages and layouts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,17 @@
 import "@/app/ui/global.css"
 import { inter } from "@/app/ui/fonts"
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+	title: {
+		template: '%s | NextInvoice',
+		default: 'NextInvoice',
+	},
+	applicationName: "NextInvoice",
+	description: "Simple open-source invoice system",
+	creator: "theGUI001",
+	metadataBase: new URL("https://nextinvoice.vercel.app/"),
+}
 
 export default function RootLayout({
 	children,
